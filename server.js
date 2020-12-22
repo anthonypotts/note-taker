1e3c0b5e933fb39761873926d9b934700d0e3104
Anthony Potts <anthonypotts@Anthonys-MacBook-Pro-2.local>
2020-12-22 16:37:50 -0600

    add heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const { notes } = require('./db/db');
+
+
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port 3001!`);
+});
+
+// heroku: pure-mesa-53928
